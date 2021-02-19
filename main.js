83760f865c040243c854681c7740b3797d4f3611
niper988 <nicolasperdomol@hotmail.com>
2021-02-19 10:28:04 -0500

    19.02.2021

--- main.js
+++ main.js
@@ -1,3 +1,74 @@
-function CambiarColor(){
-	document.getElementById('portatil').innerHTML = "a";
-}
+function CambiarColor1() {
+			bg = document.getElementById("portatil");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function CambiarColor2() {
+			bg = document.getElementById("telefono");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function CambiarColor3() {
+			bg = document.getElementById("tablet");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function CambiarColor4() {
+			bg = document.getElementById("televisor");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function CambiarColor5() {
+			bg = document.getElementById("consola");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function CambiarColor6() {
+			bg = document.getElementById("dron");
+			bg.style.fontWeight = "bold";
+			bg.style.backgroundColor = "#D8D8D8";
+		}
+
+function SalirColor1() {
+			bg = document.getElementById("portatil");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+
+function SalirColor2() {
+			bg = document.getElementById("telefono");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+
+function SalirColor3() {
+			bg = document.getElementById("tablet");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+function SalirColor4() {
+			bg = document.getElementById("televisor");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+
+function SalirColor5() {
+			bg = document.getElementById("consola");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+
+function SalirColor6() {
+			bg = document.getElementById("dron");
+			bg.style.backgroundColor = "white";
+			bg.style.fontWeight="normal";
+		}
+
+
+
+
